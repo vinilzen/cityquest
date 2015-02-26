15263b128fa1aa852617d0517276761035c22b6a
vinilzen <marchukilya@gmail.com>
2015-02-26 14:35:09 +0100

    add new quest, and fix grin on Index Page

--- protected/views/quest/index.php
+++ protected/views/quest/index.php
@@ -3,9 +3,10 @@
 /* @var $dataProvider CActiveDataProvider */
 
 $this->pageTitle= Yii::app()->name.' - '.Yii::t('app','Quests');
-
-foreach ($quests as $quest) { ?>
-	<div class="col-xs-12 col-md-6 col-sm-12 col-lg-6 col-xlg-4 item">
+$i=0;
+foreach ($quests as $quest) {
+	$i++;
+	?> <div class="col-xs-12 col-md-6 col-sm-12 col-lg-4 col-xlg-4 item">
 		<img alt="<?=CHtml::encode($quest->title)?>" class="featurette-image img-responsive" src="/images/q/<?=$quest->id?>.jpg">
 		<? if ($quest->status == 2) { ?>
 			<a class="descr" href="/quest/view?id=<? echo $quest->id; ?>">
@@ -25,9 +26,11 @@ foreach ($quests as $quest) { ?>
 				<h2><?=CHtml::encode($quest->title)?></h2>
 				<p><span class="glyphicon glyphicon-time"></span></p>
 				<p><?=Yii::t('app','The quest to develop')?></p>
-				<p><?=CHtml::encode($quest->start_text)?></p>
+				<p class="add_descr"><?=CHtml::encode($quest->start_text)?></p>
 			</a>
 		<? } ?>
 	</div>
-
+	<? if ($i%3==0) { ?>
+		<div class="clearfix visible-lg-block hidden-md"></div>
+	<? } ?>
 <? } ?>
